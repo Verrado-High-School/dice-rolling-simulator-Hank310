da96a3251bcd466af9fd186ac04e11f66be90bb1
Hank310 <56037817+Hank310@users.noreply.github.com>
2019-10-03 08:28:32 -0700

    Add files via upload

---
+++ Dice-Rolling-Simulator.py
@@ -0,0 +1,35 @@
+#Hank Warner
+#P1, Dice Rolling Sim
+import random
+1s = 0
+2s = 0
+3s = 0
+4s = 0
+5s = 0
+6s = 0
+x = 1
+RollNum = 1
+rollAmount = input("How many times should it roll?")
+randomNum = random.randint(1, 6)
+while x <= rollAmount:
+	x += 1
+	print(randomNum)
+	if randomNum == 1:
+		1s += 1
+	if randomNum == 2:
+		2s += 1
+	if randomNum == 3:
+		3s += 1
+	if randomNum == 4:
+		4s += 1
+	if randomNum == 5:
+		5s += 1
+	if randomNum == 6:
+		6s += 1
+print("Total Rolls :" + rollAmount)
+print("1s rolled :" + 1s)
+print("2s rolled :" + 2s)
+print("3s rolled :" + 3s)
+print("4s rolled :" + 4s)
+print("5s rolled :" + 5s)
+print("6s rolled :" + 6s)

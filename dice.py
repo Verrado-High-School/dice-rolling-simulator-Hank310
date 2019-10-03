9bae5eb5c05a7119a2d13320d38df71b2d371d77
Hank310 <56037817+Hank310@users.noreply.github.com>
2019-10-03 09:01:37 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -2,12 +2,12 @@
 #P1, Dice Rolling Sim
 import random
 
-1s = 0
-2s = 0
-3s = 0
-4s = 0
-5s = 0
-6s = 0
+As = 0
+bs = 0
+cs = 0
+ds = 0
+es = 0
+fs = 0
 
 x = 1
 
@@ -19,30 +19,36 @@ randomNum = random.randint(1, 6)
 
 while x <= rollAmount:
 	x += 1
-	print(randomNum)
+	print("Roll Number " + str(RollNum) + ":" + str(randomNum))
 	if randomNum == 1:
-		1s += 1
+		As += 1
 	if randomNum == 2:
-		2s += 1
+		bs += 1
 	if randomNum == 3:
-		3s += 1
+		cs += 1
 	if randomNum == 4:
-		4s += 1
+		ds += 1
 	if randomNum == 5:
-		5s += 1
+		es += 1
 	if randomNum == 6:
-		6s += 1
-print("Total Rolls :" + rollAmount)
-print("1s rolled : " + str(1s))
-print("2s rolled : " + str(2s))
-print("3s rolled : " + str(3s))
-print("4s rolled : " + str(4s))
-print("5s rolled : " + str(5s))
-print("6s rolled : " + str(6s))
+		fs += 1
+print("Total Rolls :" + str(rollAmount))
+print("1s rolled : " + str(As))
+print("2s rolled : " + str(bs))
+print("3s rolled : " + str(cs))
+print("4s rolled : " + str(ds))
+print("5s rolled : " + str(es))
+print("6s rolled : " + str(fs))
 print("Percentages : ")
-print(1s / rollAmount + "%")
-print(2s / rollAmount + "%")
-print(3s / rollAmount + "%")
-print(4s / rollAmount + "%")
-print(5s / rollAmount + "%")
-print(6s / rollAmount + "%")
+ap == As / rollAmount
+bp == bs / rollAmount
+cp == cs / rollAmount
+dp == ds / rollAmount
+ep == es / rollAmount
+fp == fs / rollAmount
+print(ap + "%")
+print(bp + "%")
+print(cp + "%")
+print(dp + "%")
+print(ep + "%")
+print(fp + "%")

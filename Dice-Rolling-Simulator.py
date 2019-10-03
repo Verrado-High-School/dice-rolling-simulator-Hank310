3fb6d465c21f8cbccea947195751c2657a5d9eba
Hank310 <56037817+Hank310@users.noreply.github.com>
2019-10-03 08:37:47 -0700

    Add files via upload

--- Dice-Rolling-Simulator.py
+++ Dice-Rolling-Simulator.py
@@ -1,16 +1,22 @@
 #Hank Warner
 #P1, Dice Rolling Sim
 import random
+
 1s = 0
 2s = 0
 3s = 0
 4s = 0
 5s = 0
 6s = 0
+
 x = 1
+
 RollNum = 1
-rollAmount = input("How many times should it roll?")
+
+rollAmount = int(input("How many times should it roll?"))
+
 randomNum = random.randint(1, 6)
+
 while x <= rollAmount:
 	x += 1
 	print(randomNum)
@@ -27,9 +33,16 @@ while x <= rollAmount:
 	if randomNum == 6:
 		6s += 1
 print("Total Rolls :" + rollAmount)
-print("1s rolled :" + 1s)
-print("2s rolled :" + 2s)
-print("3s rolled :" + 3s)
-print("4s rolled :" + 4s)
-print("5s rolled :" + 5s)
-print("6s rolled :" + 6s)
+print("1s rolled : " + 1s)
+print("2s rolled : " + 2s)
+print("3s rolled : " + 3s)
+print("4s rolled : " + 4s)
+print("5s rolled : " + 5s)
+print("6s rolled : " + 6s)
+print("Percentages : ")
+print(1s / rollAmount + "%")
+print(2s / rollAmount + "%")
+print(3s / rollAmount + "%")
+print(4s / rollAmount + "%")
+print(5s / rollAmount + "%")
+print(6s / rollAmount + "%")

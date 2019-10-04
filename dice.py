8d64cfe5b5837717df9487cfdcdf744deccc2c17
Hank310 <56037817+Hank310@users.noreply.github.com>
2019-10-04 09:02:37 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -8,30 +8,35 @@ cs = 0
 ds = 0
 es = 0
 fs = 0
-
+Ap = 1
+bp = 1
+cp = 1
+dp = 1
+ep = 1
+fp = 1
 x = 1
 
 RollNum = 1
 
 rollAmount = int(input("How many times should it roll?"))
 
-randomNum = random.randint(1, 6)
-
 while x <= rollAmount:
+	randomNum = random.randint(1, 6)
 	x += 1
-	print("Roll Number " + str(RollNum) + ":" + str(randomNum))
+	print("Roll Number " + str(int(x)) + ":" + str(int(randomNum)))
 	if randomNum == 1:
 		As += 1
-	if randomNum == 2:
+	elif randomNum == 2:
 		bs += 1
-	if randomNum == 3:
+	elif randomNum == 3:
 		cs += 1
-	if randomNum == 4:
+	elif randomNum == 4:
 		ds += 1
-	if randomNum == 5:
+	elif randomNum == 5:
 		es += 1
-	if randomNum == 6:
+	else:
 		fs += 1
+	
 print("Total Rolls :" + str(rollAmount))
 print("1s rolled : " + str(As))
 print("2s rolled : " + str(bs))
@@ -40,15 +45,10 @@ print("4s rolled : " + str(ds))
 print("5s rolled : " + str(es))
 print("6s rolled : " + str(fs))
 print("Percentages : ")
-ap == As / rollAmount
-bp == bs / rollAmount
-cp == cs / rollAmount
-dp == ds / rollAmount
-ep == es / rollAmount
-fp == fs / rollAmount
-print(ap + "%")
-print(bp + "%")
-print(cp + "%")
-print(dp + "%")
-print(ep + "%")
-print(fp + "%")
+
+print("1s " + str(100 * As/rollAmount) + " %")
+print("2s " + str(100 * bs/rollAmount) + " %")
+print("3s " + str(100 * cs/rollAmount) + " %")
+print("4s " + str(100 * ds/rollAmount) + " %")
+print("5s " + str(100 * es/rollAmount) + " %")
+print("6s " + str(100 * fs/rollAmount) + " %")
